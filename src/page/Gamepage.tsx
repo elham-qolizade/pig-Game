bbb160b48d9fc2c48deba582f75f83e87632afc1
elham-qolizade <168982184+elham-qolizade@users.noreply.github.com>
2024-07-31 15:17:17 +0330

    start create gamepage

--- src/page/Gamepage.tsx
+++ src/page/Gamepage.tsx
@@ -10,18 +10,18 @@ const Gamepage: React.FC = () => {
     setShowRules(!showRules);
   };
 
-  const rolldice = () => {
-    const diceRoll = Math.floor(Math.random() * 6) + 1;
-  };
+  // const rolldice = () => {
+  //   const diceRoll = Math.floor(Math.random() * 6) + 1;
+  // };
 
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const player1 = searchParams.get("player1");
   const player2 = searchParams.get("player2");
   const winnerScore = searchParams.get("winnerScore");
-  const [player1Score, setPlayer1Score] = useState(0);
-  const [player2Score, setPlayer2Score] = useState(0);
-  const [currentplayer, setcurrentplayer] = useState();
+  // const [player1Score, setPlayer1Score] = useState(0);
+  // const [player2Score, setPlayer2Score] = useState(0);
+  // const [currentplayer, setcurrentplayer] = useState();
 
   console.log(player1, player2, winnerScore);
 
@@ -31,21 +31,11 @@ const Gamepage: React.FC = () => {
       <div className="container flex flex-row items-center justify-center">
         <div className="py-40 bg-red-400 px-36">
           <p>{player1}</p>
-          <h2>Player 1 Score: {player1Score}</h2>
-
-          <h2>Current Player: Player {currentplayer}</h2>
         </div>
-        <div className="py-40 bg-red-500 px-36">
-          <p>{player2}</p>
-          <h2>Player 1 Score: {player2Score}</h2>
-          <h2>Current Player: Player {currentplayer}</h2>
-        </div>
+        <div className="py-40 bg-red-500 px-36"></div>
       </div>
       <p>Winner Score: {winnerScore}</p>
-      <button
-        className="px-5 py-1 mb-3 text-white bg-red-700 rounded-lg"
-        onClick={RollDice}
-      >
+      <button className="px-5 py-1 mb-3 text-white bg-red-700 rounded-lg">
         RollDice
       </button>
       <button className="px-5 py-1 mb-3 text-white bg-red-700 rounded-lg">

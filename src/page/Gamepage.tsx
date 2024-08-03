1cc02144f5de6d547ef17fafb36130e51382f5ec
elham-qolizade <168982184+elham-qolizade@users.noreply.github.com>
2024-08-03 17:26:16 +0330

    Complete the game page code

--- src/page/Gamepage.tsx
+++ src/page/Gamepage.tsx
@@ -1,145 +1,176 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Roles from "./Roles";
-//import image
-import { useLocation } from "react-router-dom";
 import dice1 from "../assets/dice1.png";
 import dice2 from "../assets/dice2.png";
 import dice3 from "../assets/dice3.png";
 import dice4 from "../assets/dice4.png";
 import dice5 from "../assets/dice5.png";
 import dice6 from "../assets/dice6.png";
+import winimage from "../assets/winner.png";
+const diceImages = [dice1, dice2, dice3, dice4, dice5, dice6];
 
-const Gamepage: React.FC = () => {
-  //show rules
+const GamePage: React.FC = () => {
+  const player1 = "player1";
+  const player2 = "player2";
+  const winScore = 20;
   const [showRules, setShowRules] = useState(false);
+  const [diceImage, setDiceImage] = useState<string>(diceImages[0]);
+  const [player1Score, setPlayer1Score] = useState(0);
+  const [player2Score, setPlayer2Score] = useState(0);
+  const [CurrentPlayer, setCurrentPlayer] = useState(
+    Math.random() < 0.5 ? 1 : 2
+  );
+  const [currentRoundScore, setCurrentRoundScore] = useState(0);
+  const [winner, setWinner] = useState<number>();
+  const [player1Color, setPlayer1Color] = useState("white");
+  const [player2Color, setPlayer2Color] = useState("white");
+  const [wincolor, setWincolor] = useState(winimage);
+
+  useEffect(() => {
+    setPlayer1Color(CurrentPlayer === 1 ? "#CCCCCC" : "white");
+    setPlayer2Color(CurrentPlayer === 2 ? "#CCCCCC" : "white");
+  }, [CurrentPlayer]);
+
   const toggleRulesModal = () => {
     setShowRules(!showRules);
   };
-  //Show pictures with dice
-  const [diceImage, setDiceImage] = useState<string>("");
-  // Rolldice function
-  const Rolldice = () => {
+
+  const rollDice = () => {
     const diceRoll = Math.floor(Math.random() * 6) + 1;
-    const diceImages = [dice1, dice2, dice3, dice4, dice5, dice6];
+
     setDiceImage(diceImages[diceRoll - 1]);
 
     if (diceRoll === 1) {
       setCurrentRoundScore(0);
       togglePlayer();
-    } else {
-      setCurrentRoundScore(currentRoundScore + diceRoll);
+      return;
     }
+    setCurrentRoundScore((c) => c + diceRoll);
   };
-  //holdscore function
-  const holdscore = () => {
-    if (currentplayer === 1) {
-      setPlayer1Score(player1Score + currentRoundScore);
+
+  const holdScore = () => {
+    const gameOver = checkGameOver();
+    if (gameOver) return;
+
+    const total =
+      (CurrentPlayer === 1 ? player1Score : player2Score) + currentRoundScore;
+
+    if (total >= winScore) {
+      setWinner(CurrentPlayer);
+      return;
+    }
+
+    if (CurrentPlayer === 1) {
+      setPlayer1Score((s) => s + currentRoundScore);
     } else {
-      setPlayer2Score(player2Score + currentRoundScore);
+      setPlayer2Score((s) => s + currentRoundScore);
     }
     togglePlayer();
+
     setCurrentRoundScore(0);
-    checkGameOver();
-  };
-  //togglePlayer function
-  const togglePlayer = () => {
-    setcurrentplayer(currentplayer === 1 ? 2 : 1);
   };
 
   const Newgame = () => {
+    setCurrentPlayer(Math.random() < 0.5 ? 1 : 2);
     setCurrentRoundScore(0);
     setPlayer1Score(0);
     setPlayer2Score(0);
+    setWinner(undefined);
   };
-  //checkGameOver function
-  // const checkGameOver = () => {
-  //   if (player1Score >= 100 || player2Score >= 100) {
-  //     setGameOver(true);
-  //     setWinner(player1Score >= 100 ? "Player 1" : "Player 2");
-  //   }
-  // };
 
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const player1 = searchParams.get("player1");
-  const player2 = searchParams.get("player2");
-  const winnerScore = searchParams.get("winnerScore");
-
-  const [player1Score, setPlayer1Score] = useState(0);
-  const [player2Score, setPlayer2Score] = useState(0);
-  const [currentplayer, setcurrentplayer] = useState(1);
-  const [currentRoundScore, setCurrentRoundScore] = useState(0);
-
-  // const [gameOver, setGameOver] = useState(false);
-
-  // const [winner, setWinner] = useState(null);
-
-  console.log(player1, player2, winnerScore);
+  const togglePlayer = () => {
+    setCurrentPlayer(CurrentPlayer === 1 ? 2 : 1);
+    setPlayer1Color(CurrentPlayer === 1 ? "#CCCCCC" : "white");
+    setPlayer2Color(CurrentPlayer === 2 ? "#CCCCCC" : "white");
+  };
+  const checkGameOver = () => {
+    if (player1Score >= winScore || player2Score >= winScore) {
+      setWinner(player1Score >= winScore ? 1 : 2);
+      setWincolor(winimage);
+      return true;
+    }
+    return false;
+  };
 
   return (
-    <div className="bg-black">
+    <div>
       <div className="container flex flex-col items-center justify-center ">
-        <h1 className="pt-6 text-3xl text-white">PIG GAME</h1>
-        <button
-          onClick={Newgame}
-          className="px-5 py-1 mb-3 text-white translate-y-24 bg-black rounded-lg"
-        >
-          New Game
-        </button>
-        <div className="container flex flex-col items-center justify-center lg:flex-row">
-          <div className="flex flex-col items-center justify-center gap-16 py-10 bg-red-900 lg:rounded-s-2xl px-36">
-            <p className="text-5xl text-white">{player1}</p>
-            <h2 className="text-5xl text-white"> {player1Score}</h2>
-            <div className="p-3 mb-2 text-center text-white bg-red-600 rounded-lg">
-              <p>current</p>
-              {currentRoundScore}
-            </div>
-            <h2 className="text-white"> Player {currentplayer}</h2>
+        <div className="">
+          <div className="text-center bg-white rounded-md pig-game">
+            <h1 className="text-2xl font-medium text-center text-orange">
+              PIG GAME
+            </h1>
           </div>
-          <div className="flex flex-col items-center justify-center gap-16 py-10 bg-red-800 lg:rounded-e-2xl px-36">
-            <p className="text-5xl text-white">{player2}</p>
-            <h2 className="text-5xl text-white"> {player2Score}</h2>
-            <div className="p-3 mb-2 text-center text-white bg-red-600 rounded-lg">
-              <p>current</p>
-              {currentRoundScore}
-            </div>
-            <h2 className="text-white"> Player {currentplayer}</h2>
-          </div>
-        </div>
-        <div className="flex flex-row items-center gap-10 mt-10">
+
           <button
             onClick={toggleRulesModal}
-            className="px-5 py-1 mb-3 text-white bg-red-700 rounded-lg"
+            className="px-6 py-4 mb-3 font-medium text-black bg-white rounded-lg"
           >
             Rules
           </button>
         </div>
-
-        <div className="flex flex-col -translate-y-72 ">
-          <div className="w-24 -translate-y-10 ">
+        <button
+          onClick={Newgame}
+          className="py-3 mb-3 text-white translate-y-40 rounded-lg px-7 new-game-btn"
+        >
+          New Game
+        </button>
+        <div className="container flex flex-col items-center justify-center py-16 lg:flex-row">
+          <div
+            className="flex flex-col items-center justify-center gap-16 py-28 lg:rounded-s-2xl px-44"
+            style={{
+              backgroundColor: player1Color,
+              backgroundImage: winner === 1 ? `url(${wincolor})` : player1Color,
+              backgroundSize: "cover",
+            }}
+          >
+            <p className="text-5xl text-black">{player1}</p>
+            <h2 className="text-red-500 text-7xl text-red"> {player1Score}</h2>
+            <div className="p-6 mb-2 text-xl text-center text-white rounded-lg current-box">
+              <p>current</p>
+              {CurrentPlayer === 1 ? currentRoundScore : 0}
+            </div>
+          </div>
+          <div
+            className="flex flex-col items-center justify-center gap-16 px-44 py-28 lg:rounded-e-2xl "
+            style={{
+              backgroundColor: player2Color,
+              backgroundImage: winner === 2 ? `url(${wincolor})` : player2Color,
+              backgroundSize: "cover",
+            }}
+          >
+            <p className="text-5xl text-black">{player2}</p>
+            <h2 className="text-red-500 text-7xl text-red"> {player2Score}</h2>
+            <div className="p-6 mb-2 text-xl text-center text-white rounded-lg current-box ">
+              <p>current</p>
+              {CurrentPlayer === 2 ? currentRoundScore : 0}
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col -translate-y-96 ">
+          <div className="w-24 -translate-y-20 ">
             <img src={diceImage} alt="" />
           </div>
           <button
-            className="px-5 py-1 mb-3 text-white bg-black rounded-lg "
-            onClick={Rolldice}
+            className="py-3 mb-3 text-white rounded-lg px-7 bg-orange "
+            onClick={rollDice}
           >
             RollDice
           </button>
           <button
-            className="px-5 py-1 mb-3 text-white bg-black rounded-lg"
-            onClick={holdscore}
+            className="py-3 mb-3 text-white rounded-lg bg-orange"
+            onClick={holdScore}
           >
             Hold
           </button>
         </div>
-
         {showRules && (
           <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
             <div className="p-8 bg-white rounded-lg">
               <Roles />
               <button
                 onClick={toggleRulesModal}
-                className="px-4 py-2 mt-4 text-white bg-red-700 rounded-lg"
+                className="px-4 py-2 mt-4 text-white bg-[#EB4D4D] rounded-lg"
               >
                 Close
               </button>
@@ -151,4 +182,4 @@ const Gamepage: React.FC = () => {
   );
 };
 
-export default Gamepage;
+export default GamePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ export default {
     extend: {
       
     },
+    container: {
+      center: true,
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#fff',
+      'black': '#000',
+      'gray':'#CCCCCC',
+      'orange':'#f27121',
+      'red':'#EB4D4D'
+    },
   },
   plugins: [],
 }
